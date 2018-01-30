2d082e1af95744faddeed8056722ce250eb7e0fd
Andrei V. Plamada <andreiplamada@gmail.com>
2018-01-30 09:56:05 +0100

    update test_run_jnb

--- run_jnb/tests/test_run_jnb.py
+++ run_jnb/tests/test_run_jnb.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
 import io
-import os
 from ..run_jnb import main
 
 # https://stackoverflow.com/questions/22822267/python-capture-print-output-of-another-module
@@ -12,4 +11,4 @@ def test_run_jnb_command_line():
     sys.argv=['run_jnb',input_path,'-m','false','-vvv']
     main()
     output = sys.stdout.getvalue()
-    assert output[:-1]==',,,,'+os.linesep
+    assert output[:4]==',,,,'
